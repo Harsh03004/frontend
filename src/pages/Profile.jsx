57beb884ac8ab2bdccc7e8726d973565bb45f929
kavya-singh101 <madhujeet111@gmail.com>
2025-02-12 23:16:51 +0530

    Implement token refresh logic in Profile component and adjust CSS for profile layout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,55 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import "../Styles/Profile.css";
 
 const Profile = () => {
+
+  let navigate = useNavigate();
+  const checkAndRefreshToken = async () => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken && accessToken!==undefined) {
+      return;
+    }
+    try {
+      const response = await fetch(`http://localhost:3000/api/v1/users/refreshToken`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ refreshToken: localStorage.getItem('refreshToken') })
+      });
+      console.log('Response:', response);
+        console.log("------------------")
+      if (response.status === 200) {
+        const json = await response.json();
+        
+        console.log('Response JSON:', json);
+        console.log("------------------")
+        console.log("access token= "+json.data.accessToken);
+        if(json.data.accessToken===undefined){
+          navigate("/login");
+          return;
+        }
+        localStorage.setItem('accessToken', json.data.accessToken);
+        localStorage.setItem('refreshToken', json.data.refreshToken);
+        return;
+      } else {
+        navigate("/login");
+      }
+      
+    } catch (error) {
+      console.log(error);
+      navigate("/login");
+    }
+  };
+    // work like documentdidmount
+    useEffect(() => {
+      checkAndRefreshToken();
+        // eslint-disable-next-line
+    }, [])
+
+
+
   const [profileImage, setProfileImage] = useState("");
 
   const handleImageUpload = (event) => {
