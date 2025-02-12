9115102e32498f601bb64e2c01c69da42c378228
Harsh Singh <harsh.101783@stu.upes.ac.in>
2025-02-12 22:53:17 +0530

    Profile page ready

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useState } from 'react';
 import Login from "./components/login";
 import Register from "./components/register";
-import Landing from "./pages/Landing";
+import Profile from "./pages/Profile";
 import Alert from "./components/Alert";
 
 function App() {
@@ -25,7 +25,7 @@ function App() {
       <div className="container">
         <Routes>
           {/* add the route to / to landing page when its built */}
-          <Route exact path="/" element={<Landing showAlert={showAlert} />} />
+          <Route exact path="/" element={<Profile showAlert={showAlert} />} />
           <Route exact path="/login" element={<Login showAlert={showAlert} />} />
           <Route exact path="/register" element={<Register showAlert={showAlert} />} />
         </Routes>
